2d24a0340b3ee5bb44a13743db53742b6d31d00c
Alessio-Gasbarro <gasbarro.developer@gmail.com>
2025-05-07 14:25:42 +0200

    [UPD] Ciclato array comic

--- dc-comics/src/components/ComicCard.jsx
+++ dc-comics/src/components/ComicCard.jsx
@@ -3,10 +3,10 @@ const ComicsCard = (props) => {
         <>
             <div className="card">
                 <div className="card-img">
-                    <img src={props.thumb} alt="Thumbnail" />
+                    <img src={props.comic.thumb} alt="Thumbnail" />
                 </div>
                 <div className="card-txt">
-                    <span>{props.title || 'No title'}</span>
+                    <span>{props.comic.title || 'No title'}</span>
                 </div>
             </div>
         </>
--- dc-comics/src/components/Header.jsx
+++ dc-comics/src/components/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
                 <div>
                     <ul>
                         <li><a href="#">CHARACTERS</a></li>
-                        <li className='active'><a href="#">COMICS</a></li>
+                        <li><a href="#">COMICS</a></li>
                         <li><a href="#">MOVIES</a></li>
                         <li><a href="#">TV</a></li>
                         <li><a href="#">GAMES</a></li>
@@ -28,4 +28,4 @@ const Header = () => {
     )
 }
 
-export default Header
+export default Header;
